b8512a8c3d7b8dd07da183b679e4675895d8cb90
Rnveer007 <rvs523474@gmail.com>
2025-02-18 16:50:32 +0530

    form created

--- Front-End/Project-Front-End/src/App.jsx
+++ Front-End/Project-Front-End/src/App.jsx
@@ -4,11 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({
+    name: "",
+    category: "",
+    brand: "",
+    price: "",
+  })
+
+  function handleChange(e) {
+
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value })
+    // setData({ ...data, [e.target.name]: e.target.value })
+
+  }
+  console.log(data)
 
   return (
     <>
-    <h2 className='text-3xl'>Hello React</h2>
+      <form action="" className='pt-4  flex gap-10 ml-10'>
+        <input type="text" name='name' value={data.name} onChange={handleChange} placeholder='Name' className='border-2 pl-3' />
+        <input type="text" name='category' value={data.category} onChange={handleChange} placeholder='Categories' className='border-2 pl-3' />
+        <input type="text" name='brand' value={data.brand} onChange={handleChange} placeholder='Brand' className='border-2 pl-3' />
+        <input type="number" name='price' value={data.price} onChange={handleChange} placeholder='Price' className='border-2 pl-3' />
+        <button type="submit" className='border-2 px-3 py-1'>Submit</button>
+      </form>
     </>
   )
 }
